ef826253e77489aacef45c29e3b85c395404868e
Muhammad-Amankhan-10 <muhammadamankhan182@gmail.com>
2026-09-03 19:30:54 +0500

    superglobuls

--- PHP/first.php
+++ PHP/first.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INTRO OF PHP</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>INTRO OF PHP</title>
 </head>
+
 <body>
-   <h1>first php work</h1>
-    <p>Lorem ipsum dolor sit amet.</p>
+  <h1>first php work</h1>
+  <!-- <p>Lorem ipsum dolor sit amet.</p> -->
 
-    <?php 
+  <?php
 
-      Echo "<br>  this is written in php";
-      echo "<br>this is written in php";
 
-      print("<hr> this work is in print") 
-    ?>
- 
+
+  $subject = htmlspecialchars($_GET['subject']);
+  $web = htmlspecialchars($_GET['web']);
+  echo "Study $subject at $web.";
+
+
+
+
+  echo "<br>  this is written in php";
+  echo "<br>this is written in php";
+
+  print("<hr> this work is in print")
+  ?>
+
 
 
 </body>
+
 </html>
---
+++ PHP/superglobules.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superglobals</title>
+</head>
+
+<body>
+   
+    
+    <?php
+    // Superglobals are built-in variables in PHP 
+
+    // GET Superglobal
+    echo $_GET["name"];
+    echo $_GET["age"];
+
+    ?>
+</body>
+
+</html>
