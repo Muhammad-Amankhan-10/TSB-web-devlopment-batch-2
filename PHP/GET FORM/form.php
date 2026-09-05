803c03502c7f46fdcd5e74cd1a74b5c68c3a94fc
Muhammad-Amankhan-10 <muhammadamankhan182@gmail.com>
2026-09-05 19:24:29 +0500

    superglobuls

---
+++ PHP/GET FORM/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP of get form</title>
+</head>
+<body>
+    
+  <?php 
+       
+        $name = $_GET["username"];
+        echo "Apka hamare page pr swagat hai " .  $name;
+  
+  
+  ?>
+
+</body>
+</html>
